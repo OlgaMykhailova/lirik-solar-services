1dcfd9fddb6a1a2c3d33a0fe4fc3140578ba258b
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-10-29 12:53:50 +0200

    Add page files structure (#7)
    * added policy page file
    
    * added not found page file
    
    * renamed files

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,5 +6,5 @@ export default createMiddleware(routing);
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ["/", "/(uk|ru)/:path*"],
+  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
 };
