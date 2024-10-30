3be21a87e76cb29c98cc5ed5dba2f67864105ca0
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-10-30 18:08:33 +0200

    added benefits, ess, stations, manufactures files

---
+++ src/components/home/ess/Ess.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Ess() {
+  return <div>Ess</div>;
+}
---
+++ src/components/home/manufactures/Manufactures.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Manufactures() {
+  return <div>Manufactures</div>;
+}
