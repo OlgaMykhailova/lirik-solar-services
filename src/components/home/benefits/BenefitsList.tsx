e4d56d78423ac64d80664ba29ca7357c9fd52c87
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-05 19:56:43 +0200

    added benefits list

--- src/components/home/benefits/BenefitsList.tsx
+++ src/components/home/benefits/BenefitsList.tsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { useTranslations } from "next-intl";
 
 export default function BenefitsList() {
+  const t = useTranslations("benefits");
+
+  const benefitsList = [
+    {
+      title: t("individual.title"),
+      description: t("individual.description"),
+      icon: "thumbsUp.svg",
+    },
+    {
+      title: t("economy.title"),
+      description: t("economy.description"),
+      icon: "lamp.svg",
+    },
+    {
+      title: t("garanty.title"),
+      description: t("garanty.description"),
+      icon: "notes.svg",
+    },
+    {
+      title: t("eco.title"),
+      description: t("eco.description"),
+      icon: "leaf.svg",
+    },
+  ];
   return <ul>BenefitsList</ul>;
 }
