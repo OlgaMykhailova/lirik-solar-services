4b15bbb4ab923599fdda9107741ec2a07dc992ee
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-05 23:33:54 +0200

    added stages list

---
+++ src/components/home/stages/StageItem.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+interface StageItemProps {
+  stage: { title: string; description: string; icon: string };
+}
+
+export default function StageItem({ stage }: StageItemProps) {
+  const { title, description, icon } = stage;
+
+  return (
+    <li className="flex flex-col items-center gap-4 tab:w-[45%] laptop:w-[23%] h-full">
+      <Image
+        src={`/images/bgImages/${icon}.webp`}
+        alt={icon}
+        height="93"
+        width="163"
+        className="h-[93px] w-auto"
+      />
+      <div className="size-4 rounded-full bg-backgroundLight"></div>
+      <h3 className="laptop:h-16 mb-2 font-gogh text-xmdb laptop:text-mdb text-center">
+        {title}
+      </h3>
+      <p>{description}</p>
+    </li>
+  );
+}
