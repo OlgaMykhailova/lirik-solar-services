8500ebc2c2288f7ce55aa6d96213a2ef775cc158
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-05 23:43:38 +0200

    updated imports

--- src/components/home/stages/Stages.tsx
+++ src/components/home/stages/Stages.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 import Section from "@/components/shared/section/Section";
 import SectionTitle from "@/components/shared/titles/SectionTitle";
+
 import StagesList from "./StagesList";
 
 export default function Stages() {
