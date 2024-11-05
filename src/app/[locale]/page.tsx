d36bad61eb1bb688af4703e422cb424d97b456a8
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-05 18:43:43 +0200

    renamed file

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -9,7 +9,7 @@ import Services from "@/components/home/services/Services";
 import Stages from "@/components/home/stages/Stages";
 import Stations from "@/components/home/stations/Stations";
 import WriteUs from "@/components/home/writeUs/WriteUs";
-import SunspotBg from "@/components/shared/backgrounds/sunspotBg";
+import SunspotBgImages from "@/components/shared/backgrounds/SunspotBgImages";
 import Container from "@/components/shared/container/Container";
 
 export default function Home() {
@@ -17,7 +17,7 @@ export default function Home() {
     <div className="pt-[72px]">
       <Hero />
       <Container className="relative">
-        <SunspotBg />
+        <SunspotBgImages />
         <About />
         <Services />
         <Stations />
--- src/components/shared/backgrounds/SunspotBgImages.tsx
+++ src/components/shared/backgrounds/SunspotBgImages.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 
-export default function SunspotBg() {
+export default function SunspotBgImages() {
   return (
     <div className="absolute w-full h-full">
       <Image
