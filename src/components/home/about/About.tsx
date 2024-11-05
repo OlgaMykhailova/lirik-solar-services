285e981143e197eb7057b0cc7e13dbaadcbd0a33
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-05 12:21:35 +0200

    added logo

--- src/components/home/about/About.tsx
+++ src/components/home/about/About.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useTranslations } from "next-intl";
 import React from "react";
 
@@ -7,8 +8,20 @@ import SectionTitle from "@/components/shared/titles/SectionTitle";
 export default function About() {
   const t = useTranslations("about");
   return (
-    <Section>
+    <Section className="relative pt-[162px] tab:pt-[64px] laptop:pt-[80px]">
+      <Image
+        src="/images/icons/logo.svg"
+        alt="logo"
+        width="0"
+        height="0"
+        className="absolute right-[2px] tab:right-[-14px] laptop:right-[-69px] top-[-64px] 
+        laptop:top-[-106px] size-[228px] tab:size-[291px] laptop:size-[578px]"
+      />
       <SectionTitle>{t("title")}</SectionTitle>
+      <div className="laptop:text-base">
+        <p className="mb-2">{t("description.partOne")}</p>
+        <p>{t("description.partTwo")}</p>
+      </div>
     </Section>
   );
 }
--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Hero() {
-  return <section>Hero</section>;
+  return (
+    <section className="h-[618px] tab:h-[728px] bg-backgroundLight">
+      Hero
+    </section>
+  );
 }
