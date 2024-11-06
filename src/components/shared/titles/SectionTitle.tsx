782fbfcac50f48d91abe7f76f003cad7ebe4ea84
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-06 17:44:06 +0200

    updated section title styles

--- src/components/shared/titles/SectionTitle.tsx
+++ src/components/shared/titles/SectionTitle.tsx
@@ -11,7 +11,7 @@ export default function SectionTitle({
     <h2
       data-label={children}
       className={`relative mb-8 laptop:mb-12 text-lgb laptop:text-xlb font-gogh before:content-[attr(data-label)] before:absolute 
-       before:-left-6 laptop:before:-left-7 before:top-4 laptop:before:top-5 before:text-grey before:text-xxlb before:blur-[1px]
+       before:-left-6 laptop:before:-left-7 before:top-4 laptop:before:top-5 before:w-full before:text-grey before:text-xxlb before:blur-[1px]
         ${className}`}
     >
       {children}
