1e0e81c268a079ed1580990366ece4270b68c97b
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-09 13:08:37 +0200

    updated header menu styles

--- src/components/shared/header/BurgerMenuMobTab.tsx
+++ src/components/shared/header/BurgerMenuMobTab.tsx
@@ -24,14 +24,15 @@ export default function BurgerMenuMobTab({
     <div
       className={`${
         isHeaderMenuOpened ? "translate-x-0 no-doc-scroll" : "translate-x-full"
-      } absolute top-0 right-0 z-50 w-[100vw] h-[100dvh] tab:min-w-[360px] tab:h-[666px] tab:w-[40%] bg-headerGradient transition-transform duration-[600ms]`}
+      } absolute top-0 right-0 z-50 w-[100vw] h-[100dvh] tab:min-w-[360px] tab:h-[666px] tab:w-[40%] bg-headerGradient 
+      transition-transform duration-[600ms] overflow-y-auto`}
     >
       <Container>
-        <div className="flex justify-between items-center h-[72px] mb-8">
+        <div className="flex justify-between items-center h-[72px]">
           <LocaleSwitcher locale={locale as Locale} />
           <IconButton handleClick={closeMenu}>{<IconClose />}</IconButton>
         </div>
-        <div className="flex flex-col items-center gap-y-16">
+        <div className="flex flex-col items-center gap-y-16 py-8">
           <MenuList closeMenu={closeMenu} />
           <CallUsButton closeMenu={closeMenu} />
         </div>
