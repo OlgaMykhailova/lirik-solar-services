5f8cba6cd287be1c560fff4749cafb1664fcba63
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-10 18:50:16 +0200

    added write us info component

--- src/components/home/writeUs/WriteUs.tsx
+++ src/components/home/writeUs/WriteUs.tsx
@@ -3,12 +3,15 @@ import React from "react";
 
 import Section from "@/components/shared/section/Section";
 import SectionTitle from "@/components/shared/titles/SectionTitle";
+import WriteUsInfo from "./WriteUsInfo";
 
 export default function WriteUs() {
   const t = useTranslations("writeUs");
+
   return (
     <Section id="order">
       <SectionTitle>{t("title")}</SectionTitle>
+      <WriteUsInfo />
     </Section>
   );
 }
