fabf438fe787798439350e56e69a4742f52953ce
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-10 18:52:10 +0200

    Write us section (#30)
    * added messages for write us section
    
    * added write us info component
    
    * updated imports

---
+++ src/components/home/writeUs/WriteUsInfo.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import { useTranslations } from "next-intl";
+import React from "react";
+
+export default function WriteUsInfo() {
+  const t = useTranslations("writeUs");
+
+  return (
+    <div className="flex flex-col gap-y-16 tab:w-[64%] laptop:w-[49%]">
+      <div>
+        <p>{t("description.partOne")}</p>
+        <p>{t("description.partTwo")}</p>
+      </div>
+      <Image
+        src="/images/solarPanelExample.webp"
+        alt="solar panel example"
+        width="528"
+        height="501"
+        className="hidden laptop:block object-cover object-center aspect-[528/501] w-full h-[501px]"
+      />
+    </div>
+  );
+}
