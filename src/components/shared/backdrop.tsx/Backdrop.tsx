bd45eb8b97d40dd786f6e820f0afe4e55a2206d1
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-12 18:13:51 +0200

    updated backdrop

--- src/components/shared/backdrop.tsx/Backdrop.tsx
+++ src/components/shared/backdrop.tsx/Backdrop.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 interface BackdropProps {
   isVisible: boolean;
@@ -9,10 +9,25 @@ export default function Backdrop({
   isVisible = false,
   onClick,
 }: BackdropProps) {
+  useEffect(() => {
+    const handleEscape = (event: KeyboardEvent) => {
+      if (event.key === "Escape" && isVisible) {
+        onClick();
+      }
+    };
+
+    document.addEventListener("keydown", handleEscape);
+
+    // Очищення обробника при розмонтуванні компонента
+    return () => {
+      document.removeEventListener("keydown", handleEscape);
+    };
+  }, [isVisible, onClick]);
+
   return (
     <div
-      className={`fixed z-40 inset-0 bg-white bg-opacity-70 transition-opacity duration-300 ${
-        isVisible ? "opacity-100 visible" : "opacity-0 invisible"
+      className={`fixed z-40 inset-0 bg-white bg-opacity-70 transition duration-[1000ms] ${
+        isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
       }`}
       onClick={onClick}
     />
--- src/components/shared/pop-ups/NotificationPopUp.tsx
+++ src/components/shared/pop-ups/NotificationPopUp.tsx
@@ -4,6 +4,7 @@ import React, { Dispatch, SetStateAction } from "react";
 
 import { PHONE_THIRD } from "@/constants/constants";
 
+import Backdrop from "../backdrop.tsx/Backdrop";
 import NotificationBgImages from "../backgrounds/NotificationBgImages";
 import IconButton from "../buttons/IconButton";
 import IconClose from "../icons/IconClose";
@@ -24,49 +25,57 @@ export default function NotificationPopUp({
   const t = useTranslations("notifications");
 
   return (
-    <div
-      className={`${
-        isNotificationShown ? "opacity-100 visible" : "opacity-0 invisible"
-      } fixed top-0 left-0 z-50 overflow-hidden flex flex-col gap-4 w-[312px] tab:w-[496px] px-6 py-[30px] tab:p-12 bg-white rounded-[16px] tab:rounded-[24px]`}
-    >
-      {!isError && <NotificationBgImages />}
-      <div className="absolute top-2 right-2 tab:top-6 tab:right-6">
-        <IconButton handleClick={() => setIsNotificationShown(false)}>
-          {<IconClose className="size-6 tab:size-10 text-black" />}
-        </IconButton>
-      </div>
-      <h2
-        data-label={t(`${isError ? "unsuccessful" : "successful"}.title`)}
-        className={`relative laptop:mb-12 text-xmdb tab:text-lgb ${
-          isError ? "text-inputError" : "text-blue"
-        } font-gogh before:content-[attr(data-label)] before:absolute 
+    <>
+      <div
+        className={`${
+          isNotificationShown
+            ? "translate-y-1/2 opacity-100 no-doc-scroll"
+            : "translate-y-full opacity-0"
+        } fixed bottom-1/2 left-1/2 transform -translate-x-1/2 transition duration-[1000ms] ease-out z-50 overflow-hidden flex flex-col gap-4 w-[312px] tab:w-[496px] px-6 py-[30px] tab:p-12 bg-white rounded-[16px] tab:rounded-[24px]`}
+      >
+        {!isError && <NotificationBgImages />}
+        <div className="absolute top-2 right-2 tab:top-6 tab:right-6">
+          <IconButton handleClick={() => setIsNotificationShown(false)}>
+            {<IconClose className="size-6 tab:size-10 text-black" />}
+          </IconButton>
+        </div>
+        <h2
+          data-label={t(`${isError ? "unsuccessful" : "successful"}.title`)}
+          className={`relative laptop:mb-12 text-xmdb tab:text-lgb ${
+            isError ? "text-inputError" : "text-blue"
+          } font-gogh before:content-[attr(data-label)] before:absolute 
        before:-left-6 laptop:before:-left-7 before:top-4 laptop:before:top-5 before:w-full before:text-grey before:text-xmdb tab:before:text-lgb before:blur-[1px]
        `}
-      >
-        {t(`${isError ? "unsuccessful" : "successful"}.title`)}
-      </h2>
-      <p className="text-xs tab:text-baseb">
-        {t(`${isError ? "unsuccessful" : "successful"}.description`)}
-        <a
-          href={`tel:+38${PHONE_THIRD.replace(/\D/g, "")}`}
-          target="_blank"
-          rel="noopener noreferrer nofollow"
-          className={`${
-            isError ? "inline" : "hidden"
-          } outline-none text-blue border-b border-blue laptop:hover:text-blueLight focus-visible:text-blueLight 
+        >
+          {t(`${isError ? "unsuccessful" : "successful"}.title`)}
+        </h2>
+        <p className="text-xs tab:text-baseb">
+          {t(`${isError ? "unsuccessful" : "successful"}.description`)}
+          <a
+            href={`tel:+38${PHONE_THIRD.replace(/\D/g, "")}`}
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+            className={`${
+              isError ? "inline" : "hidden"
+            } outline-none text-blue border-b border-blue laptop:hover:text-blueLight focus-visible:text-blueLight 
           active:text-blueLight laptop:hover:border-blueLight focus-visible:border-blueLight active:border-blueLight transition 
           duration-300 ease-out`}
-        >
-          {PHONE_THIRD}.
-        </a>
-      </p>
-      <Image
-        src={`/images/icons/${isError ? "logoGhost" : "logo"}.svg`}
-        alt="logo"
-        width={168}
-        height={168}
-        className="mx-auto tab:size-[227px]"
+          >
+            {PHONE_THIRD}.
+          </a>
+        </p>
+        <Image
+          src={`/images/icons/${isError ? "logoGhost" : "logo"}.svg`}
+          alt="logo"
+          width={168}
+          height={168}
+          className="mx-auto tab:size-[227px]"
+        />
+      </div>
+      <Backdrop
+        isVisible={isNotificationShown}
+        onClick={() => setIsNotificationShown(false)}
       />
-    </div>
+    </>
   );
 }
