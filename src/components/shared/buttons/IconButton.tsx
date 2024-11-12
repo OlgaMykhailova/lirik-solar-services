90d72544a820ebbccff749cca24fc86078769985
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-12 19:38:25 +0200

    added hover to the icon button

--- src/components/shared/buttons/IconButton.tsx
+++ src/components/shared/buttons/IconButton.tsx
@@ -15,7 +15,8 @@ export default function IconButton({
       type="button"
       onClick={handleClick}
       className={`relative text-white outline-none before:content-[''] before:absolute before:-z-10 before:top-0 before:left-0 before:rounded-full before:size-full 
-                before:bg-blueGradient before:opacity-0 before:transition before:duration-300 before:ease-out active:before:opacity-100 ${className}`}
+                before:bg-blueGradient before:opacity-0 before:transition before:duration-300 before:ease-out active:before:opacity-100 
+                focus-visible:before:opacity-100 laptop:hover:before:opacity-100 ${className}`}
     >
       {children}
     </button>
