2b72e5dc585cadf9973800b5e79e1977eeee2bc5
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-13 22:50:17 +0200

    made refactoring write us form

--- src/components/shared/forms/WriteUsForm.tsx
+++ src/components/shared/forms/WriteUsForm.tsx
@@ -1,16 +1,16 @@
 "use client";
-import axios from "axios";
-import { ErrorMessage, Field, Form, Formik, FormikHelpers } from "formik";
-import Image from "next/image";
-import Link from "next/link";
-import { useLocale, useTranslations } from "next-intl";
-import { useState } from "react";
+import { Form, Formik, FormikHelpers } from "formik";
+import { useTranslations } from "next-intl";
+import { useState, Dispatch, SetStateAction } from "react";
 import MaskedInput from "react-text-mask";
 
 import { PHONE_NUMBER_MASK } from "@/constants/constants";
 import { WriteUsValidation } from "@/schemas/writeUsFormValidation";
 
-import MainButton from "../buttons/MainButton";
+import CustomizedInput from "./CustomizedInput";
+import FormDescription from "./FormDescription";
+import SubmitButton from "./SubmitButton";
+import { handleSubmitForm } from "@/utils/handleSubmitForm";
 
 interface ValuesWriteUsFormType {
   name: string;
@@ -20,10 +20,8 @@ interface ValuesWriteUsFormType {
 }
 
 interface WriteUsFormProps {
-  setIsError: (value: boolean | ((prev: boolean) => boolean)) => void;
-  setIsNotificationShown: (
-    value: boolean | ((prev: boolean) => boolean)
-  ) => void;
+  setIsError: Dispatch<SetStateAction<boolean>>;
+  setIsNotificationShown: Dispatch<SetStateAction<boolean>>;
 }
 
 export default function WriteUsForm({
@@ -31,7 +29,6 @@ export default function WriteUsForm({
   setIsNotificationShown,
 }: WriteUsFormProps) {
   const [isLoading, setIsLoading] = useState(false);
-  const locale = useLocale();
   const t = useTranslations("");
 
   const initialValues = {
@@ -45,44 +42,23 @@ export default function WriteUsForm({
 
   const submitForm = async (
     values: ValuesWriteUsFormType,
-    { resetForm }: FormikHelpers<ValuesWriteUsFormType>
+    formikHelpers: FormikHelpers<ValuesWriteUsFormType>
   ) => {
-    try {
-      setIsLoading(true);
-      const data =
-        `<b>Нова заявка</b>\n` +
-        `Ім'я: ${values.name.trim()}\n` +
-        `Email: ${values.email.toLowerCase().trim()}\n` +
-        `Телефон: +380${values.phone.replace(/[^\d+]/g, "")}\n` +
-        `Повідомлення: ${values.message.trim()}\n`;
-      console.log(data);
+    const data =
+      `<b>Нова заявка</b>\n` +
+      `Ім'я: ${values.name.trim()}\n` +
+      `Email: ${values.email.toLowerCase().trim()}\n` +
+      `Телефон: +380${values.phone.replace(/[^\d+]/g, "")}\n` +
+      `Повідомлення: ${values.message.trim()}\n`;
 
-      await axios({
-        method: "post",
-        url: "/api/sendDataTelegram",
-        data,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      resetForm();
-    } catch (error) {
-      setIsError(true);
-      return error;
-    } finally {
-      setIsLoading(false);
-      setIsNotificationShown(true);
-    }
+    await handleSubmitForm<ValuesWriteUsFormType>(
+      formikHelpers,
+      setIsLoading,
+      setIsError,
+      setIsNotificationShown,
+      data
+    );
   };
-
-  const labelStyles =
-    "relative flex flex-col gap-y-1 w-full text-inputLabel text-xs";
-  const fieldStyles =
-    "relative w-full h-full px-3 py-[14px] text-black placeholder-inputText border rounded-[12px] outline-none text-sm transition duration-300 ease-out";
-  const fieldWrapperStyles =
-    "relative group before:content-[''] before:absolute before:top-0 before:left-0 before:rounded-[12px] before:w-full before:h-full before:blur-[3px] before:transition before:duration-300 before:ease-out ";
-  const errorStyles =
-    "absolute bottom-[-19px] right-0 text-xxs text-inputError";
 
   return (
     <Formik
@@ -92,157 +68,47 @@ export default function WriteUsForm({
     >
       {({ errors, touched, dirty, isValid }) => (
         <Form className="flex flex-col gap-y-4 w-full h-full tab:p-12 rounded-[24px] tab:bg-white tab:shadow-base">
-          <label className={labelStyles}>
-            <p>
-              {t("forms.name")} <span className="text-inputError">*</span>
-            </p>
-            <div
-              className={`${fieldWrapperStyles} ${
-                errors.name && touched.name
-                  ? "before:bg-inputError"
-                  : "before:bg-transparent group-hover:before:bg-blueLight focus-within:before:bg-blueLight"
-              }`}
-            >
-              <Field
-                name="name"
-                type="text"
-                autoComplete="on"
-                placeholder={t("forms.namePlaceholder")}
-                className={`${fieldStyles} ${
-                  errors.name && touched.name
-                    ? "border-inputErrorLight"
-                    : "border-inputStroke focus:border-blueLight "
-                }`}
-              ></Field>
-            </div>
-            <ErrorMessage
-              name="name"
-              component="p"
-              className={errorStyles}
-            ></ErrorMessage>
-          </label>
-          <label className={labelStyles}>
-            <p>
-              {t("forms.phone")} <span className="text-inputError">*</span>
-            </p>
-            <div
-              className={`${fieldWrapperStyles} ${
-                errors.phone && touched.phone
-                  ? "before:bg-inputError"
-                  : "before:bg-transparent group-hover:before:bg-blueLight focus-within:before:bg-blueLight"
-              }`}
-            >
-              <Image
-                src="/images/icons/phonePrefix.svg"
-                alt="phone prefix"
-                width={66}
-                height={22}
-                className="absolute top-1/2 left-3 z-10 transform -translate-y-1/2"
-              />
-              <Field
-                as={MaskedInput}
-                mask={PHONE_NUMBER_MASK}
-                name="phone"
-                type="text"
-                autoComplete="on"
-                placeholder={t("forms.phonePlaceholder")}
-                className={`pl-[80px] ${fieldStyles}  ${
-                  errors.phone && touched.phone
-                    ? "border-inputErrorLight"
-                    : "border-inputStroke focus:border-blueLight"
-                }`}
-              ></Field>
-            </div>
-            <ErrorMessage
-              name="phone"
-              component="p"
-              className={errorStyles}
-            ></ErrorMessage>
-          </label>
-          <label className={labelStyles}>
-            <p>{t("forms.email")}</p>
-            <div
-              className={`${fieldWrapperStyles} ${
-                errors.email && touched.email
-                  ? "before:bg-inputError"
-                  : "before:bg-transparent group-hover:before:bg-blueLight focus-within:before:bg-blueLight"
-              }`}
-            >
-              <Field
-                name="email"
-                type="email"
-                autoComplete="on"
-                placeholder={t("forms.emailPlaceholder")}
-                className={`${fieldStyles}  ${
-                  errors.email && touched.email
-                    ? "border-inputErrorLight"
-                    : "border-inputStroke focus:border-blueLight"
-                }`}
-              ></Field>
-            </div>
-            <ErrorMessage
-              name="email"
-              component="p"
-              className={errorStyles}
-            ></ErrorMessage>
-          </label>
-
-          <label className={labelStyles}>
-            {t("forms.comment")}
-            <div
-              className={`h-[92px] ${fieldWrapperStyles} ${
-                errors.message && touched.message
-                  ? "before:bg-inputError"
-                  : "before:bg-transparent group-hover:before:bg-blueLight focus-within:before:bg-blueLight"
-              }`}
-            >
-              <Field
-                as="textarea"
-                name="message"
-                type="text"
-                autoComplete="on"
-                placeholder={t("forms.commentPlaceholder")}
-                className={`min-h-[92px] resize-none ${fieldStyles} ${
-                  errors.message && touched.message
-                    ? "border-inputErrorLight"
-                    : "border-inputStroke focus:border-blueLight"
-                }`}
-              ></Field>
-            </div>
-            <ErrorMessage
-              name="message"
-              component="p"
-              className={errorStyles}
-            ></ErrorMessage>
-          </label>
-          <div className="text-sm">
-            <div className="flex gap-x-2 w-full mb-1.5 text-xs">
-              <span className="text-inputError">*</span>
-              <p>{t("forms.requiredFields")}</p>
-            </div>
-            <p>
-              {t("forms.agreement")}
-              <Link
-                href={`policy`}
-                target="_blank"
-                locale={locale}
-                className="outline-none border-b text-blue border-blue active:text-blueLight active:border-blueLight 
-                laptop:hover:text-blueLight laptop:focus-visible:text-blueLight laptop:hover:border-blueLight 
-                laptop:focus-visible:border-blueLight transition duration-300 ease-out"
-              >
-                {t("forms.policy")}
-              </Link>
-              .
-            </p>
-          </div>
-          <MainButton
-            type="submit"
-            disabled={!(dirty && isValid) || isLoading}
-            isLoading={isLoading}
-            className="mt-6 mr-auto"
-          >
-            {t("buttons.sendMessage")}
-          </MainButton>
+          <CustomizedInput
+            fieldName="name"
+            label={t("forms.name")}
+            required={true}
+            placeholder={t("forms.namePlaceholder")}
+            errors={errors}
+            touched={touched}
+          />
+          <CustomizedInput
+            fieldName="phone"
+            label={t("forms.phone")}
+            required={true}
+            placeholder={t("forms.phonePlaceholder")}
+            errors={errors}
+            touched={touched}
+            as={MaskedInput}
+            image="/images/icons/phonePrefix.svg"
+            fieldClassName="pl-[80px]"
+            mask={PHONE_NUMBER_MASK}
+          />
+          <CustomizedInput
+            fieldName="email"
+            label={t("forms.email")}
+            required={false}
+            placeholder={t("forms.emailPlaceholder")}
+            errors={errors}
+            touched={touched}
+          />
+          <CustomizedInput
+            fieldName="message"
+            label={t("forms.comment")}
+            required={false}
+            placeholder={t("forms.commentPlaceholder")}
+            errors={errors}
+            touched={touched}
+            as="textarea"
+            wrapperClassName="h-[92px]"
+            fieldClassName="min-h-[92px] resize-none"
+          />
+          <FormDescription />
+          <SubmitButton dirty={dirty} isValid={isValid} isLoading={isLoading} />
         </Form>
       )}
     </Formik>
