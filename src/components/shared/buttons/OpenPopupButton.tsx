4fc81563124a89f6c4493a5edfe5c2d98463649c
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-13 10:17:16 +0200

    added open pop-up component

---
+++ src/components/shared/buttons/OpenPopupButton.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React, { useState } from "react";
+
+import OrderPopUp from "../pop-ups/OrderPopUp";
+import MainButton from "./MainButton";
+
+interface OpenPopupButtonProps {
+  buttonName: string;
+  className?: string;
+}
+
+export default function OpenPopupButton({
+  buttonName,
+  className = "",
+}: OpenPopupButtonProps) {
+  const [isPopUpShown, setIsPopUpShown] = useState(false);
+
+  return (
+    <>
+      <MainButton onClick={() => setIsPopUpShown(true)} className={className}>
+        {buttonName}
+      </MainButton>
+      <OrderPopUp
+        isPopUpShown={isPopUpShown}
+        setIsPopUpShown={setIsPopUpShown}
+      />
+    </>
+  );
+}
