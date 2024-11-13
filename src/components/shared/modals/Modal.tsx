55d27834193228021979dfc3aa79723d1776fbc9
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-13 12:02:34 +0200

    update modal translation

--- src/components/shared/modals/Modal.tsx
+++ src/components/shared/modals/Modal.tsx
@@ -21,7 +21,7 @@ export default function Modal({
     <div
       className={`${
         isPopUpShown
-          ? "-translate-y-[calc(50vh-50%)] opacity-100"
+          ? "-translate-y-[calc(50dvh-50%)] opacity-100"
           : "translate-y-full opacity-100"
       } fixed left-1/2 bottom-0 transform -translate-x-1/2 transition duration-[1000ms] ease-out z-50 overflow-hidden flex flex-col gap-4 
    min-w-[312px] max-w-[496px] w-[86.6%] tab:w-[496px] px-6 py-12 tab:px-12 bg-white rounded-[16px] tab:rounded-[24px] shadow-base`}
