47d130c85ed9f26629ef30d5fc4eef2f1d2817e9
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-14 13:10:11 +0200

    updated pop-up title

--- src/components/shared/modals/Modal.tsx
+++ src/components/shared/modals/Modal.tsx
@@ -24,7 +24,7 @@ export default function Modal({
           ? "-translate-y-[calc(50dvh-50%)] opacity-100"
           : "translate-y-full opacity-100"
       } fixed left-1/2 bottom-0 transform -translate-x-1/2 transition duration-[1000ms] ease-out z-50 overflow-hidden flex flex-col gap-4 
-   min-w-[312px] max-w-[496px] w-[86.6%] tab:w-[496px] max-h-screen px-6 py-12 tab:px-12 bg-white rounded-[16px] tab:rounded-[24px] shadow-base overflow-y-auto`}
+   min-w-[312px] max-w-[496px] w-[86.6%] tab:w-[496px] max-h-screen px-6 py-12 tab:px-12 bg-white rounded-[16px] tab:rounded-[24px] shadow-base`}
     >
       {!isError && <ModalBgImages />}
       <div className="absolute top-2 right-2 tab:top-6 tab:right-6">
