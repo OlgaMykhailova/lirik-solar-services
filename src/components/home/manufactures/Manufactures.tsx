db612c8cae0e3f558e34b63895fc032ef0ed442b
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-15 10:18:33 +0200

    added blue bg to the manufactures section

--- src/components/home/manufactures/Manufactures.tsx
+++ src/components/home/manufactures/Manufactures.tsx
@@ -7,22 +7,27 @@ import SectionTitle from "@/components/shared/titles/SectionTitle";
 import { PRODUCTS_LINK } from "@/constants/constants";
 
 import ManufacturesInfo from "./ManufacturesInfo";
+import Container from "@/components/shared/container/Container";
 
 export default function Manufactures() {
   const t = useTranslations("");
 
   return (
-    <Section id="products">
-      <SectionTitle>{t("manufactures.title")}</SectionTitle>
-      <ManufacturesInfo />
-      <a
-        href={PRODUCTS_LINK}
-        target="_blank"
-        rel="noopener"
-        className="inline-block mt-8 tab:mt-6 outline-none"
-      >
-        <MainButton>{t("buttons.goToProducts")}</MainButton>
-      </a>
-    </Section>
+    <div className="w-screen relative left-1/2 right-1/2 -mx-[50vw] bg-backgroundLight">
+      <Container>
+        <Section id="products" className="py-8 tab:py-16">
+          <SectionTitle>{t("manufactures.title")}</SectionTitle>
+          <ManufacturesInfo />
+          <a
+            href={PRODUCTS_LINK}
+            target="_blank"
+            rel="noopener"
+            className="inline-block mt-8 tab:mt-6 outline-none"
+          >
+            <MainButton>{t("buttons.goToProducts")}</MainButton>
+          </a>
+        </Section>
+      </Container>
+    </div>
   );
 }
