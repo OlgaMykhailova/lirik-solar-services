7d3ee9fdddf183fac574f844092af688f006a980
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-19 18:45:41 +0200

    added policy section1

--- src/app/[locale]/policy/page.tsx
+++ src/app/[locale]/policy/page.tsx
@@ -1,5 +1,20 @@
+import SectionTitle from "@/components/shared/titles/SectionTitle";
 import React from "react";
+import { useTranslations } from "next-intl";
+import Container from "@/components/shared/container/Container";
+import Section1 from "@/components/policy/Section1";
 
 export default function Policy() {
-  return <div>Policy page</div>;
+  const t = useTranslations("policyPage");
+  return (
+    <div className="pt-[112px] tab:pt-[120px] laptop:pt-[136px] pb-[81px] tab:pb-[94px] laptop:pb-[103px]">
+      <Container>
+        <SectionTitle>{t("title")}</SectionTitle>
+        <p className="laptop:w-[57.4%] mb-8 laptop:mb-12">{t("description")}</p>
+        <ul>
+          <Section1 />
+        </ul>
+      </Container>
+    </div>
+  );
 }
