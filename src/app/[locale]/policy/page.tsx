eb0140ebdb337eafe6933ccd79a7c2c5626a800f
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-19 19:01:57 +0200

    added policy section2

--- src/app/[locale]/policy/page.tsx
+++ src/app/[locale]/policy/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useTranslations } from "next-intl";
 import Container from "@/components/shared/container/Container";
 import Section1 from "@/components/policy/Section1";
+import Section2 from "@/components/policy/Section2";
 
 export default function Policy() {
   const t = useTranslations("policyPage");
@@ -11,8 +12,9 @@ export default function Policy() {
       <Container>
         <SectionTitle>{t("title")}</SectionTitle>
         <p className="laptop:w-[57.4%] mb-8 laptop:mb-12">{t("description")}</p>
-        <ul>
+        <ul className="flex flex-col gap-y-8 tab:gap-y-10">
           <Section1 />
+          <Section2 />
         </ul>
       </Container>
     </div>
--- src/components/policy/Section2.tsx
+++ src/components/policy/Section2.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import { useTranslations } from "next-intl";
 import SubTitle from "../shared/titles/SubTitle";
 
-export default function Section1() {
-  const t = useTranslations("policyPage.section1");
+export default function Section2() {
+  const t = useTranslations("policyPage.section2");
   return (
     <li className="flex flex-col gap-y-4 tab:gap-y-6 laptop:flex-row justify-between">
       <SubTitle className="laptop:w-[25.5%]">{t("title")}</SubTitle>
       <ul className="flex flex-col gap-y-4 tab:gap-y-6 laptop:w-[68.1%]">
         <li>{t("part1")}</li>
         <li>{t("part2")}</li>
+        <li>{t("part3")}</li>
+        <li>{t("part4")}</li>
       </ul>
     </li>
   );
