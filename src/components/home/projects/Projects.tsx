fc16f0eb8b73b78668ce4fad0ef46ab0820dac1d
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-19 17:52:09 +0200

    Projects slider (#57)
    * added projects photos
    
    * added projects photos
    
    * added projects slider
    
    * updated sorting imports

--- src/components/home/projects/Projects.tsx
+++ src/components/home/projects/Projects.tsx
@@ -4,12 +4,17 @@ import React from "react";
 import Section from "@/components/shared/section/Section";
 import SectionTitle from "@/components/shared/titles/SectionTitle";
 
+import ProjectsSlider from "./ProjectsSlider";
+
 export default function Projects() {
   const t = useTranslations("projects");
   return (
     <Section className="pt-[72px] tab:pt-[100px] laptop:pt-[166px]">
       <SectionTitle>{t("title")}</SectionTitle>
-      <p className="tab:w-[64.3%] laptop:w-[48.9%]">{t("description")}</p>
+      <p className="mb-8 laptop:mb-12 tab:w-[64.3%] laptop:w-[48.9%]">
+        {t("description")}
+      </p>
+      <ProjectsSlider />
     </Section>
   );
 }
