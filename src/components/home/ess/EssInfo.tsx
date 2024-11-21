dc32b6488a5a8132890cfb401920a91f1eeaf6aa
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-21 14:15:07 +0200

    Update ess section (#64)
    * updated grid buttons
    
    * updated grid buttons styles

--- src/components/home/ess/EssInfo.tsx
+++ src/components/home/ess/EssInfo.tsx
@@ -12,12 +12,12 @@ export default function EssInfo() {
 
   return (
     <div className="laptop:flex">
-      <div className="flex flex-col justify-between laptop:w-[528px]">
+      <div className="flex flex-col gap-y-[15.5%] laptop:w-[528px]">
         <p className="tab:w-[64.1%] laptop:w-[448px]">{t("description")}</p>
         <GridButtons
           isGrid={isGrid}
           setIsGrid={setIsGrid}
-          className="hidden laptop:flex mt-auto"
+          className="hidden laptop:flex"
         />
       </div>
       <EssStation isGrid={isGrid} setIsGrid={setIsGrid} />
