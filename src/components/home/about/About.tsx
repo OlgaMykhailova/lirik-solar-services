fc013cf5baab566f6684cf96ac34f1c0c68427de
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-22 17:34:09 +0200

    Animate logo about section (#66)
    * animated sun and sunspot
    
    * updated main button styles
    
    * updated sorting imports
    
    * removed animation delay
    
    * added overflow-x hidden for html
    
    * added overflow-x-clip
    
    * updated styles

--- src/components/home/about/About.tsx
+++ src/components/home/about/About.tsx
@@ -1,38 +1,25 @@
-import Image from "next/image";
 import { useTranslations } from "next-intl";
 import React from "react";
 
 import Section from "@/components/shared/section/Section";
 import SectionTitle from "@/components/shared/titles/SectionTitle";
 
+import AnimatedSun from "./AnimatedSun"; // Імпорт клієнтського компонента
+
 export default function About() {
   const t = useTranslations("about");
 
   return (
-    <div>
-      <div
-        className="absolute z-20 right-4 tab:right-[31px] top-[-64px] laptop:top-[-106px] size-[228px] 
-    tab:size-[291px] laptop:size-[578px] before:content-[''] before:absolute before:top-[46px] laptop:before:top-[96px] before:left-[-4px] tab:before:left-6 before:z-[-10] 
-    before:size-[85%] before:rounded-full before:bg-yellow before:bg-opacity-15 before:blur-[40px] laptop:before:blur-[100px]"
-      >
-        <Image
-          src="/images/icons/logo.svg"
-          alt="logo"
-          width="0"
-          height="0"
-          className="w-full h-auto"
-        />
+    <Section
+      id="about"
+      className="relative pt-[162px] tab:pt-[64px] laptop:pt-[80px] overflow-x-clip"
+    >
+      <AnimatedSun />
+      <SectionTitle className="after:top-[10px]">{t("title")}</SectionTitle>
+      <div className="tab:w-[55%] laptop:w-[49%] laptop:text-base">
+        <p className="mb-2">{t("description.partOne")}</p>
+        <p>{t("description.partTwo")}</p>
       </div>
-      <Section
-        id="about"
-        className="relative pt-[162px] tab:pt-[64px] laptop:pt-[80px]"
-      >
-        <SectionTitle className="after:top-[10px]">{t("title")}</SectionTitle>
-        <div className="tab:w-[55%] laptop:w-[49%] laptop:text-base">
-          <p className="mb-2">{t("description.partOne")}</p>
-          <p>{t("description.partTwo")}</p>
-        </div>
-      </Section>
-    </div>
+    </Section>
   );
 }
