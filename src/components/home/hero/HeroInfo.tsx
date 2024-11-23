0fac6b5a8cfd892c0f531b6e95b13643d59f9015
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-23 14:07:57 +0200

    Update hero animation (#68)
    * updated hero animation timing
    
    * updated animation delay

--- src/components/home/hero/HeroInfo.tsx
+++ src/components/home/hero/HeroInfo.tsx
@@ -15,7 +15,7 @@ export default function HeroInfo() {
     <div>
       <Container className="relative">
         <p
-          className={`font-gogh text-white text-xlbb tab:text-xxxlb laptop:text-xxxxlb transition duration-[600ms] ease-slow ${
+          className={`font-gogh text-white text-xlbb tab:text-xxxlb laptop:text-xxxxlb transition duration-[1100ms] ease-slow ${
             isVisible
               ? "opacity-100 translate-y-0"
               : "opacity-0 translate-y-[309px] tab:translate-y-[364px]"
@@ -24,7 +24,7 @@ export default function HeroInfo() {
           {t("hero.companyName")}
         </p>
         <p
-          className={`mb-4 laptop:mb-6 font-gogh text-xxmdb tab:text-xlb laptop:text-xxlb text-white transition duration-[600ms] ease-slow ${
+          className={`mb-4 laptop:mb-6 font-gogh text-xxmdb tab:text-xlb laptop:text-xxlb text-white transition duration-[1100ms] ease-slow ${
             isVisible
               ? "opacity-100 translate-y-0"
               : "opacity-0 translate-y-[309px] tab:translate-y-[364px]"
@@ -35,7 +35,7 @@ export default function HeroInfo() {
         <span
           className={`relative z-10 inline-block px-4 tab:px-[38px] laptop:px-[50px] font-gogh text-lg tab:text-xxl laptop:text-xxxl text-blue before:content-[''] before:absolute before:-z-10 before:top-[-2px] laptop:before:top-1
          before:left-0 before:rounded-full before:w-full before:h-14 tab:before:h-[88px] laptop:before:h-[98px] before:bg-white before:opacity-30
-         transition delay-300 duration-[1000ms] ease-gentle ${
+         transition delay-700 duration-[1000ms] ease-gentle ${
            isVisible
              ? "opacity-100 translate-x-0"
              : "opacity-0 translate-x-[-45px] tab:translate-x-[-95px]"
@@ -46,7 +46,7 @@ export default function HeroInfo() {
         <span
           className={`absolute z-10 left-[172px] tab:left-[356px] laptop:left-[456px] top-[160px] tab:top-[259px] laptop:top-[300px] inline-block px-4 tab:px-[38px] laptop:px-[50px] align-text-top font-gogh text-lg tab:text-xxl laptop:text-xxxl text-blue 
       before:content-[''] before:absolute before:-z-10 before:top-[-2px] laptop:before:top-1 before:left-0 before:rounded-full before:w-full before:h-14 tab:before:h-[88px] laptop:before:h-[98px]
-        before:bg-white before:opacity-30 transition delay-300 duration-[1000ms] ease-gentle ${
+        before:bg-white before:opacity-30 transition delay-700 duration-[1000ms] ease-gentle ${
           isVisible
             ? "opacity-100 translate-x-0"
             : "opacity-0 translate-x-[-45px] tab:translate-x-[-95px]"
@@ -55,7 +55,7 @@ export default function HeroInfo() {
           {t("hero.energy")}
         </span>
         <h1
-          className={`w-[291px] mt-[84px] mb-6 tab:mb-10 tab:text-base text-white transition-all duration-[600ms] ease-slow ${
+          className={`w-[291px] mt-[84px] mb-6 tab:mb-10 tab:text-base text-white transition-all duration-[1100ms] ease-slow ${
             isVisible
               ? "opacity-100 translate-y-0"
               : "opacity-0 translate-y-[309px] tab:translate-y-[364px]"
@@ -65,7 +65,7 @@ export default function HeroInfo() {
         </h1>
         <div>
           <OpenPopupButton
-            className={`transition-all duration-[600ms] ease-slow ${
+            className={`transition-all duration-[1100ms] ease-slow ${
               isVisible
                 ? "opacity-100 translate-y-0"
                 : "opacity-0 translate-y-[309px] tab:translate-y-[364px]"
