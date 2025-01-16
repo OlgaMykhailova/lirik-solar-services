0732ac516dee74097edcf28dbb4faf078ddd04bb
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-16 13:19:42 +0200

    added final step messages

--- src/components/application/finalStep/FinalStep.tsx
+++ src/components/application/finalStep/FinalStep.tsx
@@ -3,14 +3,16 @@ import React from "react";
 
 export default function FinalStep() {
   return (
-    <div className="relative flex-1 flex flex-col justify-end pt-[177px] pb-8 bg-heroGradient overflow-clip">
-      <Image
-        src="/images/bgImages/panelInstallation.svg"
-        alt="panelInstallation"
-        width="792"
-        height="494"
-        className="w-[81%] tab:w-[77.9%] laptop:w-[61.7%] h-auto mx-auto"
-      />
+    <div className="relative flex-1 flex flex-col justify-end pt-[177px] tab:pt-[205px] laptop:pt-[78px] pb-8 tab:pb-20 laptop:pb-[50px] bg-heroGradient overflow-clip">
+      <div className="container max-w-[1920px]">
+        <Image
+          src="/images/bgImages/panelInstallation.svg"
+          alt="panelInstallation"
+          width="792"
+          height="494"
+          className="w-[81%] tab:w-[77.9%] laptop:w-[61.7%] h-auto mx-auto laptop:mx-0 laptop:ml-auto"
+        />
+      </div>
     </div>
   );
 }
