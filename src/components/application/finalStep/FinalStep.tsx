3f6f2ec7b227c583c133b9dcf80347c0eb3ecd67
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-16 21:34:24 +0200

    updated final step styles

--- src/components/application/finalStep/FinalStep.tsx
+++ src/components/application/finalStep/FinalStep.tsx
@@ -22,23 +22,21 @@ export default function FinalStep() {
       className="relative flex-1 flex flex-col justify-between pt-[177px] tab:pt-[205px] 
       laptop:pt-[78px] pb-20 laptop:pb-[50px] bg-heroGradient"
     >
-      <div className="container flex-1 flex flex-col max-w-[1920px]">
-        <FinalStepInfo isVisible={isVisible} />
-        <Link
-          href="/"
-          locale={locale}
-          className="mb-8 tab:mb-6 laptop:mb-0 outline-none"
-        >
-          <MainButton
-            className={`w-fit transition duration-[1100ms] ease-slow ${
-              isVisible
-                ? "opacity-100 translate-y-0"
-                : "opacity-0 translate-y-[309px] tab:translate-y-[364px]"
-            }`}
-          >
-            {t("buttons.goToWebsite")}
-          </MainButton>
-        </Link>
+      <div className="container flex-1 flex flex-col justify-around max-w-[1920px]">
+        <div className="mb-8 tab:mb-6 laptop:mb-0">
+          <FinalStepInfo isVisible={isVisible} />
+          <Link href="/" locale={locale} className="outline-none">
+            <MainButton
+              className={`w-fit transition duration-[1100ms] ease-slow ${
+                isVisible
+                  ? "opacity-100 translate-y-0"
+                  : "opacity-0 translate-y-[309px] tab:translate-y-[364px]"
+              }`}
+            >
+              {t("buttons.goToWebsite")}
+            </MainButton>
+          </Link>
+        </div>
         <Image
           src="/images/bgImages/panelInstallation.svg"
           alt="panelInstallation"
@@ -46,7 +44,7 @@ export default function FinalStep() {
           height="494"
           priority
           className={`laptop:absolute laptop:bottom-[50px] laptop:right-[3.9%] w-[76%] tab:w-[74.3%] laptop:w-auto laptop:h-[61.6%] h-auto mx-auto 
-            laptop:mx-0 laptop:ml-auto mt-auto transition delay-[600ms] duration-[1100ms] ease-slow ${
+            laptop:mx-0 laptop:ml-auto transition delay-[600ms] duration-[1100ms] ease-slow ${
               isVisible ? "opacity-100" : "opacity-0"
             }`}
         />
