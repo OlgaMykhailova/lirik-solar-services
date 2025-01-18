117c1c8f075901c0e51d7919ad6abdb699e7f3b9
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-18 20:01:33 +0200

    added styles for the secondary button

--- src/components/shared/buttons/SecondaryButton.tsx
+++ src/components/shared/buttons/SecondaryButton.tsx
@@ -9,18 +9,20 @@ export default function SecondaryButton({
   variant = "solid",
   type = "button",
   onClick,
+  disabled = false,
 }: ButtonProps) {
   return (
     <button
       aria-label={ariaLabel}
       type={type}
+      disabled={disabled}
       onClick={onClick}
       className={`flex justify-center items-center px-4 py-3 rounded-full outline-none ${
         variant === "solid"
           ? "bg-backgroundLight"
           : "bg-none border-backgroundLight border-[1px]"
-      } text-base text-black transition duration-300 ease-out laptop:hover:bg-blueMedium focus-visible:bg-blueMedium active:bg-blueLight 
-      active:scale-95 ${className}`}
+      } text-base text-black transition duration-300 ease-out laptop:enabled:hover:bg-blueMedium focus-visible:enabled:bg-blueMedium enabled:active:bg-blueLight 
+      enabled:active:scale-95 disabled:bg-inputStroke ${className}`}
     >
       {children}
     </button>
