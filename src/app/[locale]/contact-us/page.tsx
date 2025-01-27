d3e2cc3786ce49708863fa3d4780dc07f5cc2bbf
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-27 10:48:48 +0200

    added generate page meta data to the pages

--- src/app/[locale]/contact-us/page.tsx
+++ src/app/[locale]/contact-us/page.tsx
@@ -3,6 +3,20 @@ import React from "react";
 import ContactUsSection from "@/components/contactUs/contactUsSection/ContactUsSection";
 import Hero from "@/components/contactUs/hero/Hero";
 import SunspotBgImages from "@/components/shared/backgrounds/SunspotBgImages";
+import { Locale } from "@/types/locale";
+import { generatePageMetaData } from "@/utils/generatePageMetaData";
+
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: { locale: Locale };
+}) {
+  return generatePageMetaData({
+    locale,
+    namespace: "metadata",
+    canonical: "/contact-us",
+  });
+}
 
 export default function ContactUs() {
   return (
